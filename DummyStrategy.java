d518ac50a66afdcc8818fe299ac3b9a1b6f26989
nical.silva@gmail.com <nical.silva@gmail.com@836e0621-98d0-0341-be67-09ff3834c574>
2011-05-14 20:32:11 +0000

    ajout dummy strategy

---
+++ DummyStrategy.java
@@ -0,0 +1,30 @@
+package fuzzycode;
+
+public class DummyStrategy extends Strategy {
+
+	@Override
+	public Double adequacy() {
+		// TODO Auto-generated method stub
+		return 1.0;
+	}
+
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Strategy.java
+++ Strategy.java
@@ -1,10 +1,23 @@
 package fuzzycode;
 
-public class Strategy {
-    public void run() {
-	}
+/**
+ * Interface des stagtegies.
+ */
+public abstract class Strategy {
+	/**
+	 * Appelé a l'activation de la strategie.
+	 */
+	public abstract void start();
+	/**
+	 * Appelé a la desactivation de la strategie.
+	 */
+	public abstract void stop();
+	/**
+	 * Appelé a chaque frame si la strategie est active.
+	 */
+    public abstract void run();
     /**
-     * pertinence de la strategies
+     * Pertinence de la strategie.
      */
-    public Double adequacy;
+    public abstract Double adequacy();    
 }
