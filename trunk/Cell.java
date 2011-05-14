692496a966a2467799c92f7d8c234fb2f13e4688
eponix623 <eponix623@836e0621-98d0-0341-be67-09ff3834c574>
2011-05-14 18:13:23 +0000

    Implémentation de la classe Cell.

--- trunk/Cell.java
+++ trunk/Cell.java
@@ -1,26 +1,59 @@
 public class Cell {
-    private Fruit fruit;
-    private List<Equipement> equipements;
-    private Building building;
-    private Chest chest;
-    private SugarDrop sugarDrop;
-    private Integer controlLevel;
-    private Integer dangerLevel;
-    private Integer strategicInterest;
-    private Boolean wall;
-    
-    public Boolean isOccupied();
-    public Boolean isWall();
-    
-    public Boolean hasFruit();
-    public Boolean hasEquipment();
-    public Boolean hasBuilding();
-    public Boolean hasChest();
-    public Boolean hasSugar();
-    
-    public Fruit getFruit();
-    public List<Equipement> getEquipments();
-    public Building getBuilding();
-    public Chest getChest();
-    public SugarDrop getSugar();
+	private Fruit fruit;
+	private List<Equipement> equipements;
+	private Building building;
+	private Chest chest;
+	private SugarDrop sugarDrop;
+	private Integer controlLevel;
+	private Integer dangerLevel;
+	private Integer strategicInterest;
+	private Boolean wall;
+	
+	public Boolean isOccupied() {
+		return this.isWall() || this.hasEquipement() || this.hasBuilding() || this.hasChest() || this.hasFruit();
+	}
+	
+	public Boolean isWall() {
+		return this.wall;
+	}
+	
+	public Boolean hasFruit() {
+		return this.getFruit() != null;
+	}
+	
+	public Boolean hasEquipment() {
+		return this.getEquipements().size() > 0;
+	}
+	
+	public Boolean hasBuilding() {
+		return this.getBuilding() != null;
+	}
+	
+	public Boolean hasChest() {
+		return this.getChest() != null;
+	}
+	
+	public Boolean hasSugar() {
+		return this.getSugar() != null;
+	}
+	
+	public Fruit getFruit() {
+		return this.fruit;
+	}
+	
+	public List<Equipement> getEquipments() {
+		return this.equipment;
+	}
+	
+	public Building getBuilding() {
+		return this.building;
+	}
+	
+	public Chest getChest() {
+		return this.chest;
+	}
+	
+	public SugarDrop getSugar() {
+		return this.sugarDrop;
+	}
 }
