74bfe890e13818f199a6746f12fb8e3646659452
lionel_mp40@hotmail.fr <lionel_mp40@hotmail.fr@836e0621-98d0-0341-be67-09ff3834c574>
2011-05-14 18:50:18 +0000

    hangement de nom

--- trunk/Fruit.java
+++ trunk/Fruit.java
@@ -1,5 +1,27 @@
-public class Fruit implements Actor {
-        private List<Equipement> equipements;
+public abstract class Fruit {
+        protected Integer x;
+        protected Integer y;
+        protected Integer hp;
+        protected Integer defence;
+        protected Integer speed;
+        protected Integer attack;
+        protected Integer id;
         
-        public List<Equipement> getEquipments();
+        abstract public Integer getX();
+        abstract public Integer setX();
+        
+        abstract public Integer getY();
+        abstract public Integer setY();
+        
+        abstract public Integer getHp();
+        abstract public Integer setHp();
+        
+        abstract public Integer getDefense();
+        abstract public Integer getSpeed();
+        abstract public Integer getAttack();
+        abstract public Integer getId();
+        
+        //Range de base ou range max en comptant les objets
+        abstract public Integer getRange();
+        
 }
