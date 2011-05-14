bad4e377d66705f8e3d731f9162fb9b5568fd93f
eponix623 <eponix623@836e0621-98d0-0341-be67-09ff3834c574>
2011-05-14 18:56:34 +0000

    Mise en place des set de Cell. Et demenagement chez les chinois.

--- Cell.java
+++ Cell.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Cell {
 	private Fruit fruit;
-	private List<Equipment> equipments;
+	private Map<Integer, Equipment> equipments;
 	private Building building;
 	private Chest chest;
 	private SugarDrop sugarDrop;
@@ -11,8 +11,60 @@ public class Cell {
 	private Integer strategicInterest;
 	private Boolean wall;
 	
+	public void setFruit(Fruit fruit) {
+		this.fruit = fruit;
+	}
+	
+	public void addEquipment(Integer id, Equipment equip) {
+		this.equipments.put(id, equip);
+	}
+	
+	public void rmEquipment(Integer id) {
+		this.equipments.remove(id);
+	}
+	
+	public void setBuilding(Building building) {
+		this.building = building;
+	}
+	
+	public void setChest(Chest chest) {
+		this.chest = chest;
+	}
+	
+	public void setSugarDrop(SugarDrop sugar) {
+		this.sugarDrop = sugar;
+	}
+	
+	public void setControlLevel(Integer level) {
+		this.controlLevel = level;
+	}
+	
+	public void setDangerLevel(Integer level) {
+		this.dangerLevel = level;
+	}
+	
+	public void setStrategicInterest(Integer interest) {
+		this.strategicInterest = interest;
+	}
+	
+	public void setWall(Boolean wall) {
+		this.wall = wall;
+	}
+	
+	public Integer getControlLevel() {
+		return this.controlLevel;
+	}
+	
+	public Integer getDangerLevel() {
+		return this.dangerLevel;
+	}
+	
+	public Integer getStrategicInterest() {
+		return this.strategicInterest;
+	}
+	
 	public Boolean isOccupied() {
-		return this.isWall() || this.hasEquipment() || this.hasBuilding() || this.hasChest() || this.hasFruit();
+		return this.isWall() || this.hasEquipments() || this.hasBuilding() || this.hasChest() || this.hasFruit();
 	}
 	
 	public Boolean isWall() {
@@ -23,7 +75,7 @@ public class Cell {
 		return this.getFruit() != null;
 	}
 	
-	public Boolean hasEquipment() {
+	public Boolean hasEquipments() {
 		return this.getEquipments().size() > 0;
 	}
 	
@@ -43,7 +95,7 @@ public class Cell {
 		return this.fruit;
 	}
 	
-	public List<Equipment> getEquipments() {
+	public Map<Integer, Equipment> getEquipments() {
 		return this.equipments;
 	}
 	
