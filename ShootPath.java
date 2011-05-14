60caa841983c58335c887aa401a033b674bed8ff
eponix623 <eponix623@836e0621-98d0-0341-be67-09ff3834c574>
2011-05-14 17:52:35 +0000

    Classe testant l'absence d'obstacles sur la trajectoire d'un tir.

---
+++ ShootPath.java
@@ -0,0 +1,17 @@
+public class ShootPath {
+	public static Boolean test(Proxy proxy, Integer x0, Integer y0, Integer x1, Integer y1) {
+		Integer dx = Math.abs(x1 - x0), dy=Math.abs(y1 - y0);
+		Integer sx = (x0 < x1 ? 1 : -1), sy = (y0 < y1 ? 1 : -1);
+		Integer err = dx - dy;
+		
+		while (x0 != x1 || y0 != y1) {
+			if(proxy.getCase(x0, y0).isOccupied())
+				return true;
+			Integer e2 = 2 * err;
+			if (e2 > -dy)	{err -= dy; x0 += sx;}
+			if (e2 < dx)	{err += dx; y0 += sy;}
+		}
+		return false;
+	}
+}
+
