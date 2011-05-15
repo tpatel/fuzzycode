ba889492f179af87bff3e609ae3f2e41a8f20936
thibaut.patel <thibaut.patel@836e0621-98d0-0341-be67-09ff3834c574>
2011-05-15 10:04:10 +0000

    Conflit...'

--- Sparta.java
+++ Sparta.java
@@ -37,53 +37,82 @@ public class Sparta extends Strategy {
 	@Override
 	public void run() {
 		List<Fruit> lf = availableFruits;
-
+		
 		for (int k = 0; k < lf.size(); k++) {
-			Fruit f = lf.get(k);
+		//for (int k = 0; k < 1; k++) {
+				Fruit f = lf.get(k);
 			while (f.getPa() > 0) {
 
 				Point dest = new Point();
 
 				List<Fruit> enemies = Proxy.getProxy().getFruits(false);
-				List<Node> path = null;
+				//List<Node> path = null;
+				List<Point> path = null;
 				Fruit target = null;
 				for (Fruit e : enemies) {
-					System.out.println("Enemy: " + e.x + " " + e.y);
-					dest.x = e.x;
-					dest.y = e.y;
-					List<Node> list = findPath(f.getX(), f.getY(), dest.x,
-							dest.y, true);
-					if (list != null && path != null
-							&& list.size() < path.size()) {
-						path = list;
+					System.out.println("Enemy: "+e.x+" "+e.y);
+					dest.x = e.x; dest.y = e.y;
+					//List<Node> list = findPath(f.getX(), f.getY(), dest.x, dest.y, true);
+					List<Point> list = findPath(f.getX(), f.getY(), dest.x, dest.y);
+					for(int i = 0 ; i < list.size() ; i++) {
+						System.out.println(list.get(i));
+					}
+					if ((list != null) && (path == null || list.size() < path.size())) {
+						path = list;	//System.out.println("OOMMMAGAAD");
 						target = e;
 					}
 				}
-
+				if (path != null) System.out.println("enemy spotted: "+target.x+"/"+target.y);
 				if (path == null) {
 					// On ne fait rien
 					f.setPa(f.getPa() - 1); // TODO: aller à la vitamine
 				} else {
-					if (path.size() <= f.getRange()) {
-						/*
-						 * if(f.hasEquipment()) { if
-						 * (Proxy.getProxy().useEquipment(f,
-						 * f.getEquipments().get(0), e) != Api.OK)
-						 * f.setPa(f.getPa() - 1); } else {
-						 */
-						int i = Proxy.getProxy().attack(f, target);
-						if (i < 0 || i != Api.HIT)
-							f.setPa(f.getPa() - 1);
-						// }
+					/*boolean shootable = bresenham(f.x,f.y,target.x,target.y);
+					if (path.size() <= f.getRange() && shootable) {
+						//if(f.hasEquipment()) {
+						//	if (Proxy.getProxy().useEquipment(f, f.getEquipments().get(0), e) != Api.OK) f.setPa(f.getPa() - 1);
+						//} else {
+						//if (bresenham(proxy,f.x,f.y,target.x,target.y)) {
+						System.out.println("THIS IS SPARTAAA!!");
+							int i = Proxy.getProxy().attack(f, target);
+							if (i < 0 || i != Api.HIT) f.setPa(f.getPa() - 1);
+							System.out.println(">>> "+Api.decode(i));
+							if (i>0) {
+								System.out.println("Je l'ai tué!");
+								enemies.remove(target);
+							}
+						//} else f.setPa(f.getPa() - 1);
+						//}
 					} else {
-						Integer positionX = path.get(Math.min(path.size(), f
-								.getSpeed())
-								- f.getRange()).x;
-						Integer positionY = path.get(Math.min(path.size(), f
-								.getSpeed())
-								- f.getRange()).y;
+						System.out.println("moving...");
+						int ind = Math.min(path.size()-1, Math.max(f.getSpeed()-f.getRange()-1+(shootable?0:1),0));
+						//System.out.println(":::::::::::"+ind);
+						Integer positionX = path.get(ind).x;
+						Integer positionY = path.get(ind).y;
 						if (Proxy.getProxy().move(f, positionX, positionY) != Api.OK)
 							f.setPa(f.getPa() - 1);
+					}
+					*/
+					//System.out.println(path.size()+" "+(f.getSpeed() - 1));
+					if (path.size()==0) {
+						int i = Proxy.getProxy().attack(f, target);
+						if (i < 0 || i != Api.HIT) f.setPa(f.getPa() - 1);
+						System.out.println(">>> "+Api.decode(i)+"  "+i);
+						if (i>=0) {
+							System.out.println("Je l'ai tué!");
+							enemies.remove(target);
+							
+						}
+					} else {
+						Integer r = 0, i = 1;
+						do {
+							Integer positionX = path.get(Math.max(0, Math.min(path.size(), f.getSpeed()) - i)).x;
+							Integer positionY = path.get(Math.max(0, Math.min(path.size(), f.getSpeed()) - i)).y;
+							r = Proxy.getProxy().move(f, positionX, positionY);
+							System.out.println("" + positionX + " " + positionY);
+							i++;
+						} while(r == Api.TOO_FAR && i < 42);
+						if(r != Api.OK)	f.setPa(f.getPa() - 1);
 					}
 				}
 			}
--- Strategy.java
+++ Strategy.java
@@ -1,5 +1,6 @@
 package fuzzycode;
 
+import java.awt.Point;
 import java.util.*;
 
 /**
@@ -7,7 +8,6 @@ import java.util.*;
  */
 public abstract class Strategy {
 	public Strategy() {
-		this.proxy = Proxy.getProxy();
 		this.oncePerFrame = true;
 		this.cachedAdequacy = new AdequacyResult(0.0, new ArrayList<Fruit>());
 	}
@@ -71,7 +71,7 @@ public abstract class Strategy {
 
 	// static final int INFINITY = Integer.MAX_VALUE;
 	static final int[][] neighbourPattern = { { -1, -1 }, { 0, -1 }, { 1, -1 },{ -1, 0 }, { 1, 0 }, { -1, 1 }, { 0, 1 }, { 1, 1 } };
-	protected Proxy proxy;
+
 
 	@SuppressWarnings("unchecked")
 	protected class Node implements Comparable {
@@ -90,24 +90,30 @@ public abstract class Strategy {
 			return d - ((Node) arg0).d;
 		}
 	}
+	
+	protected int distHeuristique(int x1, int y1, int x2, int y2) {
+		return Math.max(Math.abs(x1 - x2), Math.abs(y1 - y2));
+	}
 
-	@SuppressWarnings("unchecked")
+	/*@SuppressWarnings("unchecked")
 	protected List<Node> findPath(int x1, int y1, int x2, int y2,
 			boolean building) { // System.out.println("isOcc "+proxy.getCell(0,0).isOccupied());
-		int w = proxy.getMapWidth();
-		int h = proxy.getMapHeight();
+		int w = Proxy.getProxy().getMapWidth();
+		int h = Proxy.getProxy().getMapHeight();
 		Node[][] aNodes = new Node[w][h];
 		List<Node> nodes = new ArrayList<Node>();
 		for (int i = 0; i < w; i++) {
 			for (int j = 0; j < h; j++) {
 				aNodes[i][j] = new Node(i, j);
 				nodes.add(aNodes[i][j]);
-				aNodes[i][j].isPenetrable = !proxy.getCell(i, j).isOccupied();
+				aNodes[i][j].isPenetrable = !Proxy.getProxy().getCell(i, j).isOccupied();
 			}
 		}
 		aNodes[x1][y1].d = 0;
 		Node end = null;
-		while (nodes.size() != 0) {
+		Integer  t= 0;
+		while (nodes.size() != 0 && t < 200) {
+			t++;
 			Collections.sort(nodes);
 			Node u = nodes.get(0);
 
@@ -151,20 +157,27 @@ public abstract class Strategy {
 				nodes.remove(nodes.size() - 1);
 			return nodes;
 		}
+	}*/
+
+	protected List<Point> findPath(int x1, int y1, int x2, int y2) {
+		AStar as = new AStar(new Point(x1, y1), new Point(x2, y2), 10);
+		return as.calculPath();
 	}
 	
-	public static Boolean bresenham(Proxy proxy, Integer x0, Integer y0, Integer x1, Integer y1) {
+	
+	
+	public static Boolean bresenham(Integer x0, Integer y0, Integer x1, Integer y1) {
 		int dx=Math.abs(x1-x0), dy=Math.abs(y1-y0);
 		int sx = (x0<x1?1:-1), sy = (y0<y1?1:-1);
 		int err = dx-dy;
 		while (x0!=x1 || y0!=y1) {
-			if(proxy.getCell(x0, y0).isOccupied())
-				return true;
+			if(Proxy.getProxy().getCell(x0, y0).isOccupied())
+				return false;
 			int e2 = 2*err;
 			if (e2>-dy) {err -= dy; x0 += sx;}
 			if (e2<dx) {err += dx; y0 += sy;}
 			}
-		return false;
+		return true;
 	}
 	
 
@@ -206,6 +219,135 @@ public abstract class Strategy {
 			}
 		}
 
+	}
+	
+	public static class AStar {
+		public static Integer distHeur(Point p0, Point p1) {
+			return Math.max(Math.abs(p1.x - p0.x), Math.abs(p1.y - p0.y));
+		}
+		
+		private List<Point> closedList;
+		private List<Point> openList;
+		private Map<Point, Point> visitedList;
+		
+		private Map<Point, Integer> distHeur;
+		private Map<Point, Integer> distReal;
+		private Map<Point, Integer> distEstim;
+		
+		private Point begin;
+		private Point end;
+		private Integer range;
+		
+		public AStar(Point begin, Point end, Integer range) {
+			this.closedList = new ArrayList<Point>();
+			this.openList = new ArrayList<Point>();
+			this.visitedList = new HashMap<Point, Point>();
+			
+			this.distEstim = new HashMap<Point, Integer>();
+			this.distReal = new HashMap<Point, Integer>();
+			this.distHeur = new HashMap<Point, Integer>();
+			
+			this.distHeur.put(begin, distHeur(begin, end));
+			this.distReal.put(begin, 0);
+			this.distEstim.put(begin, distHeur(begin, end));
+			
+			this.begin = begin;
+			this.end = end;
+			this.range = range;
+			
+			this.openList.add(this.begin);
+		}
+		
+		public List<Point> calculPath() {
+			while(!this.openList.isEmpty()) {
+				Integer min = this.distEstim.get(openList.get(0));
+				Point pMin = openList.get(0);
+				/*for(Map.Entry<Point, Integer> e : distEstim.entrySet()) {
+					if(e.getValue() < min) {
+						min = e.getValue();
+						pMin = e.getKey();
+					}
+				}*/
+				for (int i = 0; i < this.openList.size(); i++) {
+					for(Map.Entry<Point, Integer> e : distEstim.entrySet()) {
+						if(openList.get(i).x == e.getKey().x && openList.get(i).y == e.getKey().y && e.getValue() < min) {
+							min = e.getValue();
+							pMin = e.getKey();
+							//System.out.println("min");
+						}
+					}
+				}
+				
+				if(distHeur(pMin, this.end) <= this.range){
+					System.out.println(pMin);
+					return AStar.getPath(this.visitedList, this.visitedList.get(begin));
+				}
+				
+				//System.out.print(" ( " + this.openList.size() + " " + this.closedList.size() + " " + distEstim.size() + "|");
+				this.closedList.add(pMin);
+				//boolean retour = this.openList.remove(pMin);
+				int ind = -1;
+				for (int j = 0; j < this.openList.size(); j++) {
+					if(openList.get(j).x == pMin.x && openList.get(j).y == pMin.y) {
+						ind = j;
+					}
+				}
+				Point retour = new Point(42, 42);
+				if(ind >=0) retour = this.openList.remove(ind);
+				//System.out.print(" | " + this.openList.size() + " " + this.closedList.size() + ") " + retour);
+				
+				Point[] neighbours = {new Point(pMin.x - 1, pMin.y), new Point(pMin.x, pMin.y - 1), new Point(pMin.x - 1, pMin.y - 1), new Point(pMin.x + 1, pMin.y), new Point(pMin.x, pMin.y + 1), new Point(pMin.x + 1, pMin.y + 1), new Point(pMin.x - 1, pMin.y + 1), new Point(pMin.x + 1, pMin.y - 1)};
+				for(int i = 0 ; i < 8 ; i++) {
+					//if(this.closedList.contains(neighbours[i]))	continue;
+					boolean result = false;
+					for (int j = 0; j < this.closedList.size(); j++) {
+						if(closedList.get(j).x == neighbours[i].x && closedList.get(j).y == neighbours[i].y) {
+							result = true;
+							break;
+						}
+					}
+					if(result) continue;
+					if(Proxy.getProxy().getCell(neighbours[i].x, neighbours[i].y).isOccupied())	continue;
+					Integer distReal = this.distReal.get(pMin) + 1; //Par la suite on pourra influencer le trajet par la presence d'alliés ou d'ennemis
+					
+					Boolean pass = false;
+					for (int j = 0; j < this.openList.size(); j++) {
+						if(openList.get(j).x == neighbours[i].x && openList.get(j).y == neighbours[i].y) {
+							pass = true;
+							break;
+						}
+					}
+					if(!pass) {
+						openList.add(neighbours[i]);
+						pass = true;
+					}
+					else if(distReal < this.distReal.get(neighbours[i]))
+						pass = true;
+					
+					if(pass) {
+						this.visitedList.put(neighbours[i], pMin);
+						this.distReal.put(neighbours[i], distReal);
+						this.distHeur.put(neighbours[i], distHeur(neighbours[i], this.end));
+						this.distEstim.put(neighbours[i], this.distReal.get(neighbours[i]) + this.distHeur.get(neighbours[i]));
+					}
+				}
+			}
+			
+			return null;
+		}
+		
+		public static List<Point> getPath(Map<Point, Point> visitedList, Point currentNode) {
+			if(visitedList.get(currentNode) != null) {
+				List<Point> l = (getPath(visitedList, visitedList.get(currentNode)));
+				l.add(currentNode);
+				return l;
+			}
+			else {
+				List<Point> l = new ArrayList<Point>();
+				l.add(currentNode);
+				return l;
+			}
+		}
 	}
 
 }
