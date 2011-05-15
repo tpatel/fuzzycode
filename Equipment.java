d875247c2092e0b20f9891fff996b82cb80f059a
rnow.if@gmail.com <rnow.if@gmail.com@836e0621-98d0-0341-be67-09ff3834c574>
2011-05-15 02:48:27 +0000

    Correction des bugs dans constructure Equipment qui faisait un pointeur null

--- Equipment.java
+++ Equipment.java
@@ -11,6 +11,9 @@ public class Equipment {
 	protected Integer id;
 	protected Integer type;
 	protected Integer ammoMax;
+	protected Integer sellValue;
+	protected Integer buyValue;
+	protected Double ammoValue;
 
 	public Integer getAttack() {
 		return attack;
@@ -61,81 +64,150 @@ public class Equipment {
 		return this.weightBase + this.weightAmmo * this.ammo;
 	}
 
+	public Double getWeightBase() {
+		return weightBase;
+	}
+
+	public void setWeightBase(Double weightBase) {
+		this.weightBase = weightBase;
+	}
+
+	public Double getWeightAmmo() {
+		return weightAmmo;
+	}
+
+	public void setWeightAmmo(Double weightAmmo) {
+		this.weightAmmo = weightAmmo;
+	}
+
+	public Integer getSellValue() {
+		return sellValue;
+	}
+
+	public void setSellValue(Integer sellValue) {
+		this.sellValue = sellValue;
+	}
+
 	public Equipment(Integer type) {
 
+		this.ammo = ammoMax;
+		this.type = type;
+		
 		if (this.type == Api.EQUIPMENT_CUTTER) {
 			this.attack = 8;
 			this.range = 2;
 			this.ammoMax = -1;
 			this.weightBase = 15.0;
 			this.weightAmmo = 0.0;
+			this.ammoValue = 0.0;
+			this.buyValue = 50;
+			this.sellValue = 30;
+			
 		}
 
-		if (this.type == Api.EQUIPMENT_JUICE_NEEDLE) {
+		else if (this.type == Api.EQUIPMENT_JUICE_NEEDLE) {
 			this.attack = 4;
 			this.range = 1;
 			this.ammoMax = 10;
 			this.weightBase = 2.0;
 			this.weightAmmo = 1.0;
+			this.ammoValue = 2.0;
+			this.buyValue = 30;
+			this.sellValue = 25;
 		}
 
-		if (this.type == Api.EQUIPMENT_LEMONER) {
+		else if (this.type == Api.EQUIPMENT_LEMONER) {
 			this.attack = 4;
 			this.range = 6;
 			this.ammoMax = 25;
 			this.weightBase = 10.0;
 			this.weightAmmo = 0.4;
+			this.ammoValue = 0.2;
+			this.buyValue = 35;
+			this.sellValue = 20;
 		}
 
-		if (this.type == Api.EQUIPMENT_LIGHTER) {
+		else if (this.type == Api.EQUIPMENT_LIGHTER) {
 			this.attack = 2;
 			this.range = 5;
 			this.ammoMax = 100;
 			this.weightBase = 11.0;
 			this.weightAmmo = 0.01;
+			this.ammoValue = 0.02;
+			this.buyValue = 15;
+			this.sellValue = 10;
 		}
 
-		if (this.type == Api.EQUIPMENT_PEELER) {
+		else if (this.type == Api.EQUIPMENT_PEELER) {
 			this.attack = -1;
 			this.range = 2;
 			this.ammoMax = -1;
 			this.weightBase = 5.0;
 			this.weightAmmo = 0.0;
+			this.ammoValue = 0.0;
+			this.buyValue = 15;
+			this.sellValue = 10;
 		}
 
-		if (this.type == Api.EQUIPMENT_RELOADER) {
+		else if (this.type == Api.EQUIPMENT_RELOADER) {
 			this.attack = 100;
 			this.range = 1;
 			this.ammoMax = 1;
 			this.weightBase = 2.0;
 			this.weightAmmo = 10.0;
+			this.ammoValue = 10.0;
+			this.buyValue = 20;
+			this.sellValue = 15;
 		}
 
-		if (this.type == Api.EQUIPMENT_SALT_SNIPER) {
+		else if (this.type == Api.EQUIPMENT_SALT_SNIPER) {
 			this.attack = 6;
 			this.range = 10;
 			this.ammoMax = 10;
 			this.weightBase = 20.0;
 			this.weightAmmo = 1.0;
+			this.ammoValue = 0.5;
+			this.buyValue = 60;
+			this.sellValue = 40;
 		}
 
-		if (this.type == Api.EQUIPMENT_TEA_SPOON) {
+		else if (this.type == Api.EQUIPMENT_TEA_SPOON) {
 			this.attack = 4;
 			this.range = 1;
 			this.ammoMax = -1;
 			this.weightBase = 5.0;
 			this.weightAmmo = 0.0;
+			this.ammoValue = 0.0;
+			this.buyValue = 10;
+			this.sellValue = 6;
 		}
 
-		if (this.type == Api.EQUIPMENT_TOOTHPICK) {
+		else if (this.type == Api.EQUIPMENT_TOOTHPICK) {
 			this.attack = 6;
 			this.range = 1;
 			this.ammoMax = -1;
 			this.weightBase = 10.0;
 			this.weightAmmo = 0.0;
+			this.ammoValue = 0.0;
+			this.buyValue = 20;
+			this.sellValue = 12;
 		}
 
-		this.ammo = ammoMax;
-		this.type = type;
+	}
+
+	public Integer getBuyValue() {
+		return buyValue;
+	}
+
+	public void setBuyValue(Integer buyValue) {
+		this.buyValue = buyValue;
+	}
+
+	public Double getAmmoValue() {
+		return ammoValue;
+	}
+
+	public void setAmmoValue(Double ammoValue) {
+		this.ammoValue = ammoValue;
 	}
 }
