9c695552bb63ed90c9c24ba7cf91803f9e956ec8
nical.silva@gmail.com <nical.silva@gmail.com@836e0621-98d0-0341-be67-09ff3834c574>
2011-05-15 07:30:12 +0000

    ajout de fonctions utilitaires

---
+++ trunk/RockBand.java
@@ -0,0 +1,85 @@
+package fuzzycode;
+
+import game.Api;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RockBand extends Strategy {
+	private List<Fruit> availableFruits = new ArrayList<Fruit>();
+
+	@Override
+	public AdequacyResult computeAdequacy(List<Fruit> availableFruits) {
+		
+		Fruit closestEnemy = closestFruit(
+				(int)Math.round( proxy.getProxy().getBuildingNeutral(Api.BUILDING_VITAMIN_SOURCE).getX() )
+				, (int)Math.round( proxy.getProxy().getBuildingNeutral(Api.BUILDING_VITAMIN_SOURCE).getX() )
+				, proxy.getProxy().getFruits(false));
+		
+		
+		
+		this.availableFruits.addAll(availableFruits);
+		return new AdequacyResult(1.0, availableFruits);
+	}
+
+	@Override
+	public void run() {
+		List<Fruit> lf = availableFruits;
+
+		for (int k = 0; k < lf.size(); k++) {
+			Fruit f = lf.get(k);
+			while (f.getPa() > 0) {
+				
+				Integer typeBuilding = null;
+				if (f.getCurVitamins() == 0) {
+					typeBuilding = Api.BUILDING_VITAMIN_SOURCE;
+				} else {
+					typeBuilding = Api.BUILDING_SUGAR_BOWL;
+				}
+				Point dest = null;
+					
+				if (typeBuilding == Api.BUILDING_VITAMIN_SOURCE) {
+					dest = Proxy.getProxy().getBuildingNeutral(typeBuilding);					
+				} else {
+					dest = Proxy.getProxy().getBuildingFriend(typeBuilding);
+				}
+				
+				List<Node> list = findPath(f.getX(), f.getY(), dest.x, dest.y, true);
+				if (list == null) {
+					// On ne fait rien
+					f.setPa(f.getPa() - 1);
+				} else {
+					if (list.size() == 0) {
+						if (typeBuilding == Api.BUILDING_VITAMIN_SOURCE) {
+							Proxy.getProxy().drawVitamin(f);
+						}
+						else 
+							if (Proxy.getProxy().stockSugar(f) != Api.OK) f.setPa(f.getPa() - 1);
+					} else {
+						Integer positionX = list.get(Math.min(list.size(), f.getSpeed()) - 1).x;
+						Integer positionY = list.get(Math.min(list.size(), f.getSpeed()) - 1).y;
+						if (Proxy.getProxy().move(f, positionX, positionY) != Api.OK)
+							f.setPa(f.getPa() - 1);
+					}
+
+				}
+
+			}
+
+		}
+	}
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
